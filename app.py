afd83feccfec894a9284893aeb7d10d47ee0111f
saepudin91 <119606890+saepudin91@users.noreply.github.com>
2025-04-25 10:07:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ import gspread
 from google.oauth2.service_account import Credentials
 import streamlit as st
 
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-credentials = Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes=scope)
-gc = gspread.authorize(credentials)
-
-# Akses sheet
-sheet = gc.open("Data Portofolio").worksheet("Pesan Pengunjung")
+def connect_sheet(sheet_name):
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    
+    credentials = Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"], scopes=scope
+    )
+    client = gspread.authorize(credentials)
+    sheet = client.open("Data Portofolio").worksheet(Pesan Pengunjung)
+    return sheet
